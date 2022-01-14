7957e43a86e6a5d0f226c8bd537af5ac0a1c8aa9
nirzaf <mfmfazrin1986@gmail.com>
2022-01-14 12:22:57 +0530

    Add project files.

---
+++ Address.cs
@@ -0,0 +1,21 @@
+﻿public class Address : ICloneable
+{
+    public readonly string StreetName;
+    public int HouseNumber;
+
+    public Address(string streetName, int houseNumber)
+    {
+        StreetName = streetName;
+        HouseNumber = houseNumber;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+    }
+
+    public object Clone()
+    {
+        return new Address(StreetName, HouseNumber);
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿public class Person : ICloneable
+{
+    public readonly string[] Names;
+    public readonly Address Address;
+
+    public Person(string[] names, Address address)
+    {
+        Names = names;
+        Address = address;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+    }
+
+    public object Clone()
+    {
+        return new Person(Names, Address);
+    }
+}
